64c7a51ddcb84e2ffae81428b32abb2e150c793b
rom <mozzieisme@gmail.com>
2021-11-08 20:43:03 -0500

    Add new ejs file and add new code to server.js

--- express_server.js
+++ express_server.js
@@ -34,6 +34,13 @@ app.get("/urls", (req, res) => {
 
 
 
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL };
+  res.render("urls_show", templateVars);
+});
+
+
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
